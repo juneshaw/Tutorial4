7dff73e6e483c4e94dbca90deb686827e0db3bd8
June Shaw <june.shaw@me.com>
2016-05-11 13:18:37 -0600

    initial commit

---
+++ src/Application.java
@@ -0,0 +1,13 @@
+
+public class Application {
+	public static void main(String[] args) {
+		boolean myBool = 5<6;
+		System.out.println(myBool);
+		
+		int myNumber=10;
+		while (myNumber > 0) {
+			System.out.println("Hello " + myNumber);
+			myNumber--;
+		}
+	}
+}
